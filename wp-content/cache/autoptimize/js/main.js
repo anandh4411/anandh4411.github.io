70305eb083ab745f954948f1c3af207fc3fac722
Anandh <74340629+anandh4411@users.noreply.github.com>
2021-09-20 21:55:08 +0530

    light theme

---
+++ wp-content/cache/autoptimize/js/main.js
@@ -0,0 +1,6 @@
+import smoothscroll from "smoothscroll-polyfill";
+// or if linting/typescript complains
+import * as smoothscroll from "smoothscroll-polyfill";
+
+// kick off the polyfill!
+smoothscroll.polyfill();
